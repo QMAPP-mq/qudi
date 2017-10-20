7a31fd06e38743070ef2de75f2d8c6fd1f3fc4b4
Lachlan J Rogers <lachlan.j.rogers@quantum.diamonds>
2017-10-20 11:46:35 +1100

    Initial commit to branch. Started work on USB PI piezo scanner.

---
+++ hardware/motor/piezo_stage_pi_usb_gcs2.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains the hardware control for PI piezo stages running GCS2.
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+import time
+from ctypes import *
+
+from collections import OrderedDict
+
+from core.module import Base, ConfigOption
+from interface.motor_interface import MotorInterface
+
+
+class PiezoStagePI(Base, MotorInterface):
+    """unstable: Lachlan Rogers, Matt van Breugel
+    This is the hardware module for communicating with PI Piezo scanning stages
+    over USB (via the PI dll). It uses the General Command Set 2 (GCS2) from
+    the PI documentation.
+
+    This module has been developed for the E-725 Digital Piezo Controller,
+    but probably works with any PI controller that talks GCS2 over USB.
+    """
+    _modclass = 'PiezoStagePI'
+    _modtype = 'hardware'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_activate(self):
+        """ Initialisation performed during activation of the module.
+        @return: error code
+        """
+
+        # TODO: connect to USB device
+
+        return 0
+
+    def on_deactivate(self):
+        """ Deinitialisation performed during deactivation of the module.
+        @return: error code
+        """
+
+        return 0
+
+    def get_constraints(self):
+        """ Retrieve the hardware constrains from the motor device.
+
+        @return dict: dict with constraints for the sequence generation and GUI
+
+        Provides all the constraints for the xyz stage  and rot stage (like total
+        movement, velocity, ...)
+        Each constraint is a tuple of the form
+            (min_value, max_value, stepsize)
+        """
+        constraints = OrderedDict()
+
+        return constraints
+
+    def move_rel(self, param_dict):
+        """Moves stage in given direction (relative movement)
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+
+
+        @return dict pos: dictionary with the current magnet position
+        """
+
+        return param_dict
+
+    def move_abs(self, param_dict):
+        """Moves stage to absolute position
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                parameters, which should be changed. Usage:
+                                 {'axis_label': <the-abs-pos-value>}.
+                                 'axis_label' must correspond to a label given
+                                 to one of the axis.
+                                The values for the axes are in millimeter,
+                                the value for the rotation is in degrees.
+
+        @return dict pos: dictionary with the current axis position
+        """
+
+        return param_dict
+
+    def abort(self):
+        """Stops movement of the stage
+
+        @return int: error code (0:OK, -1:error)
+        """
+        return 0
+
+    def get_pos(self, param_list=None):
+        """ Gets current position of the stage arms
+
+        @param list param_list: optional, if a specific position of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the
+                                position is asked.
+
+        @return dict: with keys being the axis labels and item the current
+                      position.        """
+        return param_dict
+
+    def get_status(self, param_list=None):
+        """ Get the status of the position
+
+        @param list param_list: optional, if a specific status of an axis
+                                is desired, then the labels of the needed
+                                axis should be passed in the param_list.
+                                If nothing is passed, then from each axis the
+                                status is asked.
+
+        @return dict: with the axis label as key and the status number as item.
+        The meaning of the return value is:
+        Bit 0: Ready Bit 1: On target Bit 2: Reference drive active Bit 3: Joystick ON
+        Bit 4: Macro running Bit 5: Motor OFF Bit 6: Brake ON Bit 7: Drive current active
+        """
+
+    def calibrate(self, param_list=None):
+        """ Calibrates the stage.
+
+        @param dict param_list: param_list: optional, if a specific calibration
+                                of an axis is desired, then the labels of the
+                                needed axis should be passed in the param_list.
+                                If nothing is passed, then all connected axis
+                                will be calibrated.
+
+        After calibration the stage moves to home position which will be the
+        zero point for the passed axis.
+
+        @return dict pos: dictionary with the current position of the ac#xis
+        """
+
+        return pos
+
+    def get_velocity(self, param_list=None):
+        """ Gets the current velocity for all connected axes in m/s.
+
+        @param list param_list: optional, if a specific velocity of an axis
+                                    is desired, then the labels of the needed
+                                    axis should be passed as the param_list.
+                                    If nothing is passed, then from each axis the
+                                    velocity is asked.
+
+        @return dict : with the axis label as key and the velocity as item.
+            """
+
+
+    def set_velocity(self, param_dict):
+        """ Write new value for velocity in m/s.
+
+        @param dict param_dict: dictionary, which passes all the relevant
+                                    parameters, which should be changed. Usage:
+                                     {'axis_label': <the-velocity-value>}.
+                                     'axis_label' must correspond to a label given
+                                     to one of the axis.
+
+        @return dict param_dict2: dictionary with the updated axis velocity
+        """
+
+
+
+
+########################## internal methods ##################################
+
+    def _do_move_abs(self, axis, move):
+        """internal method for the absolute move in meter
+
+        @param axis string: name of the axis that should be moved
+
+        @param float move: desired position in meter
+
+        @return str axis: axis which is moved
+                move float: absolute position to move to
+        """
+        constraints = self.get_constraints()
+        #self.log.info(axis + 'MA{0}'.format(int(move*1e8)))
+        if not(constraints[axis]['pos_min'] <= move <= constraints[axis]['pos_max']):
+            self.log.warning('Cannot make the movement of the axis "{0}"'
+                'since the border [{1},{2}] would be crossed! Ignore command!'
+                ''.format(axis, constraints[axis]['pos_min'], constraints[axis]['pos_max']))
+        else:
+            self._write_xyz(axis,'MA{0}'.format(int(move*1e7)))  # 1e7 to convert meter to SI units
+            #self._write_xyz(axis, 'MP')
+        return axis, move
+
+
+#########################################################################################
+
+
+
+
+
+
+
