c7f1c5fcd14c3aeae7b7b5668a9b8ddd2deda4c0
mvbnano <matt.vanbreugel@gmail.com>
2018-05-30 16:27:52 +1000

    mechanics for setting the colour of the wavelength representor from the powermeter detection wavelength

--- gui/powermeter/powermetergui.py
+++ gui/powermeter/powermetergui.py
@@ -30,6 +30,7 @@ from gui.guibase import GUIBase
 from qtpy import QtCore
 from qtpy import QtWidgets
 from qtpy import uic
+from qtpy import QtGui
 
 
 
@@ -372,3 +373,73 @@ class PowermeterGui(GUIBase):
     def update_smoothing_ComboBox(self):
         self.log.warning('Not implemented yet')
         return 0
+
+    def _set_wavelength_representor(self, wavelength):
+        """ Set the colour of the wavelength representor
+
+            @param float wavelength : in meters
+        """
+
+        self._mw.wavelength_representor.setAutoFillBackground(True)
+
+        palette = self._mw.wavelength_representor.palette()
+
+        r, g, b = self._wavelength_to_rgb(wavelength)
+
+        palette.setColor(self._mw.wavelength_representor.backgroundRole(), QtGui.QColor(r, g, b))
+
+        self._mw.wavelength_representor.setPalette(palette)
+
+    def _wavelength_to_rgb(self, wavelength, gamma=0.8):
+
+        '''This converts a given wavelength of light to an 
+        approximate RGB color value. The wavelength must be given
+        in nanometers in the range from 380 nm through 750 nm
+        (789 THz through 400 THz).
+
+        Based on code by Dan Bruton
+        http://www.physics.sfasu.edu/astro/color/spectra.html
+        
+            @param wavelength : input wavelenght in meters
+            
+            @returns tuple : (red value, green value, blue value)
+        '''
+
+        wavelength = wavelength / 1e-9
+        
+        wavelength = float(wavelength)
+        if wavelength >= 380 and wavelength <= 440:
+            attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+            R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+            G = 0.0
+            B = (1.0 * attenuation) ** gamma
+        elif wavelength >= 440 and wavelength <= 490:
+            R = 0.0
+            G = ((wavelength - 440) / (490 - 440)) ** gamma
+            B = 1.0
+        elif wavelength >= 490 and wavelength <= 510:
+            R = 0.0
+            G = 1.0
+            B = (-(wavelength - 510) / (510 - 490)) ** gamma
+        elif wavelength >= 510 and wavelength <= 580:
+            R = ((wavelength - 510) / (580 - 510)) ** gamma
+            G = 1.0
+            B = 0.0
+        elif wavelength >= 580 and wavelength <= 645:
+            R = 1.0
+            G = (-(wavelength - 645) / (645 - 580)) ** gamma
+            B = 0.0
+        elif wavelength >= 645 and wavelength <= 750:
+            attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+            R = (1.0 * attenuation) ** gamma
+            G = 0.0
+            B = 0.0
+        else:
+            R = 0.0
+            G = 0.0
+            B = 0.0
+        R *= 255
+        G *= 255
+        B *= 255
+        
+        return (int(R), int(G), int(B))
