f1e3b019f835e38ce14bffd529e5ed81140e472d
mvbnano <matt.vanbreugel@gmail.com>
2018-07-31 14:52:12 +1000

    correctly show the name of the hardware module class in the example config

--- hardware/motor/piezo_stage_pi_pci_gcs.py
+++ hardware/motor/piezo_stage_pi_pci_gcs.py
@@ -43,7 +43,7 @@ class PiezoStagePI_GCS1(Base, MotorInterface):
     Example configuration:
     ```
     # pi_piezo:
-    #     module.Class: 'motor.piezo_stage_pi_pci_gcs.PiezoStagePI'
+    #     module.Class: 'motor.piezo_stage_pi_pci_gcs.PiezoStagePI_GCS1'
     #     axis_labels:
     #         - x
     #         - y
